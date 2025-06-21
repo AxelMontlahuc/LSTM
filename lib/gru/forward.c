677a04b16b2f249bdc87c6c5f88b03334ae2baa1
AxelMontlahuc <axel.montlahuc@proton.me>
2025-06-21 23:05:47 +0200

    Added untested forward pass

--- lib/gru/forward.c
+++ lib/gru/forward.c
@@ -10,23 +10,23 @@ double sigmoid(double x) {
 }
 
 double* resetGate(GRU* model, double* x) {
-    double* z = calloc(model->hiddenSize, sizeof(double));
-    assert(z != NULL);
+    double* r = calloc(model->hiddenSize, sizeof(double));
+    assert(r != NULL);
 
     for (int i=0; i<model->hiddenSize; i++) {
         for (int j=0; j<model->inputSize; j++) {
-            z[i] += model->Wrx[i][j] * x[j];
+            r[i] += model->Wrx[i][j] * x[j];
         }
 
         for (int j=0; j<model->hiddenSize; j++) {
-            z[i] += model->Wrh[i][j] * model->h[j];
+            r[i] += model->Wrh[i][j] * model->h[j];
         }
 
-        z[i] += model->Br[i];
-        z[i] = sigmoid(z[i]);
+        r[i] += model->Br[i];
+        r[i] = sigmoid(r[i]);
     }
 
-    return z;
+    return r;
 }
 
 double* updateGate(GRU* model, double* x) {
@@ -67,4 +67,40 @@ double* candidateState(GRU* model, double* x, double* r) {
     }
 
     return z;
+}
+
+double* forward(GRU* model, WeatherData* data, int idx) {
+    double* x = malloc(model->inputSize * sizeof(double));
+    assert(x != NULL);
+
+    x[0] = data->date[idx];
+    x[1] = data->temp[idx];
+    x[2] = data->humidity[idx];
+    x[3] = data->windSpeed[idx];
+    x[4] = data->pressure[idx];
+
+    double* r = resetGate(model, x);
+    double* z = updateGate(model, x);
+    double* g = candidateState(model, x, r);
+
+    for (int i=0; i<model->hiddenSize; i++) {
+        model->h[i] = (1 - z[i]) * model->h[i] + z[i] * g[i];
+    }
+
+    free(x);
+    free(r);
+    free(z);
+    free(g);
+
+    double* output = calloc(model->outputSize, sizeof(double));
+    assert(output != NULL);
+
+    for (int i=0; i<model->outputSize; i++) {
+        for (int j=0; j<model->hiddenSize; j++) {
+            output[i] += model->Wo[i][j] * model->h[j];
+        }
+        output[i] += model->Bo[i];
+    }
+
+    return output;
 }
